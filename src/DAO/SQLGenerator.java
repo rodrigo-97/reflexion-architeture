aa02a45ec2de553b816f815607ac95682e6e2d95
Diego Teixeira <ogeidexe@gmail.com>
2021-01-13 20:08:59 -0300

    Criação da classe de geração de sql

---
+++ src/DAO/SQLGenerator.java
@@ -0,0 +1,15 @@
+package DAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLGenerator {
+    private String tableName;
+    private List<String> atributesNames =  new ArrayList();
+    private List<Object>  atributesValues = new ArrayList();
+
+    public SQLGenerator(){
+
+    }
+
+}
